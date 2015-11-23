494d437812c44bcb9c05da72769886fcf537bf94
lg <leo@tripndrive.com>
2015-11-23 21:06:23 +0100

    pom & rest file

---
+++ src/main/java/com/tripndrive/shuttle/TndShuttleBackApplication.java
@@ -0,0 +1,12 @@
+package com.tripndrive.shuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TndShuttleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TndShuttleBackApplication.class, args);
+    }
+}
